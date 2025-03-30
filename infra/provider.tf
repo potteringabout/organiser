0d8943f63a3edc2ed9d895da42217ca71fa348fd
AntonyPotter <tone@potteringabout.net>
2025-03-30 16:09:58 +0000

    Fixed WAF stuff

--- infra/provider.tf
+++ infra/provider.tf
@@ -13,6 +13,25 @@ terraform {
 provider "aws" {
   alias  = "us_east_1"
   region = "us-east-1"
+
+  assume_role {
+    role_arn = var.deployment_role_arn
+  }
+
+  default_tags {
+    tags = {
+      account          = var.account
+      account_full     = var.account_full
+      deployment_mode  = var.deployment_mode
+      deployment_repo  = var.deployment_repo
+      email            = var.email
+      environment      = var.environment
+      environment_full = var.environment_full
+      owner            = var.owner
+      project          = var.project
+      project_full     = var.project_full
+    }
+  }
 }
 
 provider "aws" {
