ff4acb01dc9813c2e372857eb89b7a88dc40d8ac
AntonyPotter <tone@potteringabout.net>
2025-03-30 18:31:52 +0000

    Added cert to cloudfront

--- infra/route53.tf
+++ infra/route53.tf
@@ -43,7 +43,7 @@ resource "aws_route53_record" "cert_validation" {
   provider = aws.dns
 }
 
-resource "aws_acm_certificate_validation" "cf_cert_validation" {
+resource "aws_acm_certificate_validation" "cert_validation" {
   provider                = aws.us_east_1
   certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
