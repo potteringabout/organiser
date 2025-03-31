00ef3edc6ed07b6295ebd341132d43f6dd718067
AntonyPotter <tone@potteringabout.net>
2025-03-31 21:16:46 +0000

    Added logging to the API gw

--- infra/apigw.tf
+++ infra/apigw.tf
@@ -147,16 +147,16 @@ resource "aws_api_gateway_stage" "stage" {
   access_log_settings {
     destination_arn = aws_cloudwatch_log_group.api_gw_logs.arn
     format = jsonencode({
-      requestId       = "$context.requestId",
-      ip              = "$context.identity.sourceIp",
-      caller          = "$context.identity.caller",
-      user            = "$context.identity.user",
-      requestTime     = "$context.requestTime",
-      httpMethod      = "$context.httpMethod",
-      resourcePath    = "$context.resourcePath",
-      status          = "$context.status",
-      protocol        = "$context.protocol",
-      responseLength  = "$context.responseLength"
+      requestId      = "$context.requestId",
+      ip             = "$context.identity.sourceIp",
+      caller         = "$context.identity.caller",
+      user           = "$context.identity.user",
+      requestTime    = "$context.requestTime",
+      httpMethod     = "$context.httpMethod",
+      resourcePath   = "$context.resourcePath",
+      status         = "$context.status",
+      protocol       = "$context.protocol",
+      responseLength = "$context.responseLength"
     })
   }
 
