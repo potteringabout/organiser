e74c30e8d1b2c01e98812c4b330ecacd598baa43
AntonyPotter <tone@potteringabout.net>
2025-04-02 11:09:20 +0000

    Added Cw Queries

--- infra/cwqueries.tf
+++ infra/cwqueries.tf
@@ -2,7 +2,7 @@ resource "aws_cloudwatch_query_definition" "query_errors" {
   name = "High Error Count in Logs"
 
   log_group_names = [
-    "/aws/api-gateway/${var.project}-${var.environment}",
+    "/aws/apigateway/${var.project}-${var.environment}",
     "/aws/lambda/${var.project}-${var.environment}-lambda",
     "/${var.project}/${var.environment}/cloudfront"
   ]
