cab1aaf11279e5bb1bafaa89cd123bf9736f529c
Antony Potter <tone@potteringabout.net>
2025-04-21 13:21:19 +0000

    Updated lambda package

--- lambda/app/package.sh
+++ lambda/app/package.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+PYTHON_VERSION=3.13
+# This script packages the app and its dependencies into a zip file
+# that can be deployed to AWS Lambda.
+# It requires Docker to be installed.
+# The script assumes that the current working directory is the root of the project.
+# It is important that the project is built using the same Python version as the Lambda runtime.
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 sudo rm -rf package
+rm lambda_package.zip
 docker run --rm -v "$PWD":/var/task \
   --entrypoint "" \
-  public.ecr.aws/lambda/python:3.13 \
+  public.ecr.aws/lambda/python:${PYTHON_VERSION} \
   bash -c "pip install -r requirements.txt --platform manylinux2014_x86_64 --target ./package --only-binary=:all:"
 
 cd package
