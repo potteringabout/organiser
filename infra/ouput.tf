b2a5bdeb62d8f72301e98eae1b8dd4fecd74119e
Antony Potter <tone@potteringabout.net>
2025-05-18 21:15:02 +0000

    Removed dynamo

--- infra/ouput.tf
+++ infra/ouput.tf
@@ -41,6 +41,6 @@ output "ec2_instance_id" {
 
 
 output "rds_connect_command" {
-  value = var.create_ec2_instance ? "To connect to the RDS instance, run the following command in your terminal:\n\n aws ssm start-session --target ${aws_instance.linux[0].id} --document-name AWS-StartPortForwardingSessionToRemoteHost --parameters '{\"host\":[\"${aws_db_instance.rds.address}\"],\"portNumber\":[\"5432\"],\"localPortNumber\":[\"5432\"]}'\n\nUsername: dbadmin\nDatabase: organiser\nPassword: See secrets manager ( ${var.project}-${var.environment}-postgres-creds)" : null
+  value       = var.create_ec2_instance ? "To connect to the RDS instance, run the following command in your terminal:\n\n aws ssm start-session --target ${aws_instance.linux[0].id} --document-name AWS-StartPortForwardingSessionToRemoteHost --parameters '{\"host\":[\"${aws_db_instance.rds.address}\"],\"portNumber\":[\"5432\"],\"localPortNumber\":[\"5432\"]}'\n\nUsername: dbadmin\nDatabase: organiser\nPassword: See secrets manager ( ${var.project}-${var.environment}-postgres-creds)" : null
   description = "The command to connect to the RDS instance"
 }
