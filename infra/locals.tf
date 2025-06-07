26121651849a838946a0313f3c8d2c4701e993b7
Antony Potter <tone@potteringabout.net>
2025-06-07 12:42:38 +0000

    Moved locals around

--- infra/locals.tf
+++ infra/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  fqdn           = var.environment == "prod01" ? var.project : "${var.project}-${var.environment}.${var.zone}"
+  fqdn           = var.environment == "prod01" ? "${var.project}.${var.zone} : "${var.project}-${var.environment}.${var.zone}"
   db_credentials = jsondecode(data.aws_secretsmanager_secret_version.postgres_creds.secret_string)
 }
