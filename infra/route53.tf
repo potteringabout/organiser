746436151f7e4f9a1ac8d52a84049c46918e1f3d
Antony Potter <tone@potteringabout.net>
2025-08-29 20:47:24 +0000

    Using v6 region support

--- infra/route53.tf
+++ infra/route53.tf
@@ -45,7 +45,7 @@ resource "aws_route53_record" "cert_validation" {
 }
 
 resource "aws_acm_certificate_validation" "cert_validation" {
-  region                  = "us-east-1"  
+  region                  = "us-east-1"
   certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
